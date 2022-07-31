a129e34267b2b76f203bf570f48158dd92f68a22
Laura Kovačić <laurakciic@gmail.com>
2022-07-31 08:56:07 +0200

    design RecipesBottomSheet (meal and diet types)

---
+++ Foodie_RMA/app/src/main/java/com/example/foodie/ui/fragments/recipes/bottomSheet/RecipesBottomSheet.kt
@@ -0,0 +1,21 @@
+package com.example.foodie.ui.fragments.recipes.bottomSheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import foodie.R
+
+class RecipesBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+    }
+
+
+}
