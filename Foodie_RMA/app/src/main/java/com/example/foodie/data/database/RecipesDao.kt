26e281690bcd385e4c264d501314dfb72ab94a80
Laura Kovačić <laurakciic@gmail.com>
2022-09-11 20:35:13 +0200

    modify RecipesDao interface

--- Foodie_RMA/app/src/main/java/com/example/foodie/data/database/RecipesDao.kt
+++ Foodie_RMA/app/src/main/java/com/example/foodie/data/database/RecipesDao.kt
@@ -1,9 +1,7 @@
 package com.example.foodie.data.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import com.example.foodie.data.database.entities.FavoritesEntity
 import com.example.foodie.data.database.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -14,8 +12,20 @@ interface RecipesDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)         // when fetching new data, it will replace the old ones
     suspend fun insertRecipes(recipesEntity: RecipesEntity)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
     // return type is Flow, List of RecipesEntity wrapped inside Flow
     // Flow in RecipesDao, Repository, in ViewModel converting Flow to LiveData
     @Query("SELECT * FROM recipes_table ORDER BY id ASC")
     fun readRecipes(): Flow<List<RecipesEntity>>
+
+    @Query("SELECT * FROM favorite_recipes_table ORDER BY id ASC")
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>>
+
+    @Delete
+    suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+    @Query("DELETE FROM favorite_recipes_table")
+    suspend fun deleteAllFavoriteRecipes()
 }
