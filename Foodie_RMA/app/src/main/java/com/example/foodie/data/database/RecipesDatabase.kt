6c15d32cfa48dcdbb91398b332548f5cc13da697
Laura Kovačić <laurakciic@gmail.com>
2022-09-11 20:50:47 +0200

    modify RecipesDatabase class

--- Foodie_RMA/app/src/main/java/com/example/foodie/data/database/RecipesDatabase.kt
+++ Foodie_RMA/app/src/main/java/com/example/foodie/data/database/RecipesDatabase.kt
@@ -3,10 +3,11 @@ package com.example.foodie.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.foodie.data.database.entities.FavoritesEntity
 import com.example.foodie.data.database.entities.RecipesEntity
 
 @Database(
-    entities = [RecipesEntity::class],
+    entities = [RecipesEntity::class, FavoritesEntity::class],
     version = 1,                        // needs to be incremented whenever changing schema
     exportSchema = false
 )
