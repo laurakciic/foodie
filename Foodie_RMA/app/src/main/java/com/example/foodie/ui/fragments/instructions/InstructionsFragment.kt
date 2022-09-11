8237812bd687c0ff07c118768a98e77cbb81c176
Laura Kovačić <laurakciic@gmail.com>
2022-09-11 18:40:04 +0200

    modify InstructionsFragment + solve http error

--- Foodie_RMA/app/src/main/java/com/example/foodie/ui/fragments/instructions/InstructionsFragment.kt
+++ Foodie_RMA/app/src/main/java/com/example/foodie/ui/fragments/instructions/InstructionsFragment.kt
@@ -1,20 +1,40 @@
 package com.example.foodie.ui.fragments.instructions
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import foodie.R
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.example.foodie.models.Result
+import com.example.foodie.util.Constants
+import foodie.databinding.FragmentInstructionsBinding
 
 class InstructionsFragment : Fragment() {
+
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        binding.instructionsWebView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        binding.instructionsWebView.loadUrl(websiteUrl)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
