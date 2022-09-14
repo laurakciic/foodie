a38b53b589e1fc5de43c1ade06149348109a4afe
Laura Kovačić <laurakciic@gmail.com>
2022-09-14 10:10:24 +0200

    display favorite recipes + fix bug: recipe overlapping with system UI

--- Foodie_RMA/app/src/main/java/com/example/foodie/ui/fragments/favourites/FavouriteRecipesFragment.kt
+++ Foodie_RMA/app/src/main/java/com/example/foodie/ui/fragments/favourites/FavouriteRecipesFragment.kt
@@ -1,21 +1,50 @@
 package com.example.foodie.ui.fragments.favourites
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import foodie.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodie.adapters.FavoriteRecipesAdapter
+import com.example.foodie.viewModels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import foodie.databinding.FragmentFavouriteRecipesBinding
 
+@AndroidEntryPoint  // bc MainViewModel is using dependency injection
 class FavouriteRecipesFragment : Fragment() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+    private val mAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter(requireActivity(), mainViewModel) }
+
+    private var _binding: FragmentFavouriteRecipesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourite_recipes, container, false)
+        _binding = FragmentFavouriteRecipesBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
+        binding.mAdapter = mAdapter
+
+        setHasOptionsMenu(true)
+
+        mainViewModel.readFavoriteRecipes.observe(viewLifecycleOwner) { favoritesEntity ->
+            mAdapter.setData(favoritesEntity)
+        }
+
+        setupRecyclerView(binding.favoriteRecipesRecyclerView)
+
+        return binding.root
     }
 
-
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = mAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
 }
