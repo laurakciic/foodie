e5cc203fd054e114d40944b0186a16c7d8b13c89
Laura Kovačić <laurakciic@gmail.com>
2022-09-15 15:00:52 +0200

    create FoodFact model class

--- Foodie_RMA/app/src/main/java/com/example/foodie/models/FoodFact.kt
+++ Foodie_RMA/app/src/main/java/com/example/foodie/models/FoodFact.kt
@@ -4,6 +4,9 @@ package com.example.foodie.models
 import com.google.gson.annotations.SerializedName
 
 data class FoodFact(
+
+    // no need to create type converters bc we can store this value directly in our db
+
     @SerializedName("text")
     val text: String
 )
