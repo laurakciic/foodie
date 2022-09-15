a0636dcdf8893a6d56264bc3342fb5a01bcef370
Laura Kovačić <laurakciic@gmail.com>
2022-09-15 15:05:01 +0200

    create FoodFact GET request

--- Foodie_RMA/app/src/main/java/com/example/foodie/data/network/FoodRecipesAPI.kt
+++ Foodie_RMA/app/src/main/java/com/example/foodie/data/network/FoodRecipesAPI.kt
@@ -1,18 +1,21 @@
 package com.example.foodie.data.network
 
+import com.example.foodie.models.FoodFact
 import com.example.foodie.models.FoodRecipe
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.QueryMap
 
 interface FoodRecipesAPI {
 
-    // simple function that takes queries inside the parametres
+    // simple function that takes queries inside the parameters
     // - annotated with GET annotation in which we specified an endpoint
     //   which we use to receive data from an API
     // - suspend because our fun will use Kotlin Coroutines
     // - it will also run on a background thread instead of a main thread
-    // QueryMap - all queries inside one map
+    // QueryMap - all queries inside one map (multiple endpoints)
+    // Query - only one endpoint
     // FoodRecipe is wrapped inside of Response class which is part of Retrofit library
     @GET("/recipes/complexSearch")
     suspend fun getRecipes(@QueryMap queries: Map<String, String>): Response<FoodRecipe>
@@ -20,4 +23,6 @@ interface FoodRecipesAPI {
     @GET("/recipes/complexSearch")
     suspend fun searchRecipes(@QueryMap searchQuery: Map<String, String>): Response<FoodRecipe>
 
+    @GET("food/trivia/random")
+    suspend fun getFoodFact(@Query("apiKey") apiKey: String): Response<FoodFact>
 }
