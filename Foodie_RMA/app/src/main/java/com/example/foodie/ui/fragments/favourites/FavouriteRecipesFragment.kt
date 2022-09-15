47c2db3fe2fa347f7bd3ddafb7515d2693ed6748
Laura Kovačić <laurakciic@gmail.com>
2022-09-15 09:53:45 +0200

    create favorite_menu_item so user can delete all favorite recipes at once

--- Foodie_RMA/app/src/main/java/com/example/foodie/ui/fragments/favourites/FavouriteRecipesFragment.kt
+++ Foodie_RMA/app/src/main/java/com/example/foodie/ui/fragments/favourites/FavouriteRecipesFragment.kt
@@ -1,16 +1,16 @@
 package com.example.foodie.ui.fragments.favourites
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodie.adapters.FavoriteRecipesAdapter
 import com.example.foodie.viewModels.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import foodie.R
 import foodie.databinding.FragmentFavouriteRecipesBinding
 
 @AndroidEntryPoint  // bc MainViewModel is using dependency injection
@@ -32,11 +32,32 @@ class FavouriteRecipesFragment : Fragment() {
         binding.mainViewModel = mainViewModel
         binding.mAdapter = mAdapter
 
-        setHasOptionsMenu(true)
+        setHasOptionsMenu(true)  // shows menu in action bar
 
         setupRecyclerView(binding.favoriteRecipesRecyclerView)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.favorite_recipes_menu, menu)    // inflater to inflate menu
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.deleteAll_favorite_recipes_menu){    // delete all fav recipes
+            mainViewModel.deleteAllFavoriteRecipes()
+            showSnackBar()  // no params since fun is here used only once so the message is in impl
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showSnackBar(){
+        Snackbar.make(
+            binding.root,
+            "All recipes removed.",
+            Snackbar.LENGTH_SHORT
+        ).setAction("Okay"){}
+            .show()
     }
 
     private fun setupRecyclerView(recyclerView: RecyclerView) {
